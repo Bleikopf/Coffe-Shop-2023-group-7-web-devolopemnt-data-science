681f8b75aa915fad3be642e66acb962e40a81f81
Bleikopf <paranoidpenguinbleilo@gmail.com>
2023-03-18 23:42:50 +0100

    Update script.js
    Signed-off-by: Bleikopf <paranoidpenguinbleilo@gmail.com>

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,7 +1,8 @@
 const FIGURE = $("#figure");
+const NAME = 'Coffe-Shop-2023-group-7-web-devolopemnt-data-science/'
 
 function loadMap(name) {
-    FIGURE.load(`../tree/maps/${name}.html`);
+    FIGURE.load(`../${name}maps/${name}.html`);
 }
 
 loadMap('25sqm2emp');
